ebe28cae56e54a98069881db087f3ba9d6900ef2
Prince <pm11prince@gmail.com>
2018-08-03 14:30:29 +0530

    removed all sql files

--- V2__change.sql
+++
@@ -1,9 +0,0 @@
--- -----------------------------------------------------
--- Table `MigrationType`
--- -----------------------------------------------------
-CREATE TABLE `MigrationType` (
-  `MigrationTypeId` INT NOT NULL AUTO_INCREMENT,
-  `TypeName` VARCHAR(100) NULL,
-  PRIMARY KEY (`MigrationTypeId`),
-  UNIQUE INDEX `MigrationTypeId_UNIQUE` (`MigrationTypeId` ASC));
-  
